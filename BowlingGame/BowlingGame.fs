5c322d690ad8b2651b57dc887fa2a7b1abe4016c
Henrik Ebbeskog <henrik.ebbeskog@activesolution.se>
2020-12-30 07:46:45 +0100

    Attempt at pattern match instead.

--- BowlingGame/BowlingGame.fs
+++ BowlingGame/BowlingGame.fs
@@ -5,12 +5,6 @@ type Game() =
     let mutable currentRoll = 0
     // Maximum of 21 rolls in a bowling game:
     let rolls : int array = Array.zeroCreate 21 
-    
-    let isSpare frameIndex =
-        rolls.[frameIndex] + rolls.[frameIndex + 1] = 10
-    
-    let isStrike frameIndex =
-        rolls.[frameIndex] = 10
 
     member self.Roll(pins : int) =
         rolls.[currentRoll] <- pins
@@ -19,19 +13,24 @@ type Game() =
 
     member self.Score() =
         let mutable score = 0
-        let mutable frameIndex = 0
+        let mutable rollIndex = 0
         // Ten frames in a bowling game
         [|1..10|]
         |> Array.iter (fun _ -> 
-            if frameIndex |> isStrike then
-                score <- score + 10 + rolls.[frameIndex + 1] + rolls.[frameIndex + 2]
-                frameIndex <- frameIndex + 1
-            else if  frameIndex |> isSpare then
-                score <- score + 10 + rolls.[frameIndex + 2] 
-                frameIndex <- frameIndex + 2
-            else
-                score <- score + rolls.[frameIndex] + rolls.[frameIndex + 1]
-                frameIndex <- frameIndex + 2
+            match rolls.[rollIndex], rolls.[rollIndex + 1], rolls.[rollIndex + 2] with
+            // Strike?
+            | 10, firstRollAfterStrike, secondRollAfterStrike -> 
+                score <- score + 10 + firstRollAfterStrike + secondRollAfterStrike
+                rollIndex <- rollIndex + 1
+            // Spare?
+            | 1, 9, rollAfterSpare | 2, 8, rollAfterSpare | 3, 7, rollAfterSpare | 4, 6, rollAfterSpare 
+            | 5, 5, rollAfterSpare 
+            | 9, 1, rollAfterSpare | 8, 2, rollAfterSpare | 7, 3, rollAfterSpare | 6, 4, rollAfterSpare ->
+                score <- score + 10 + rollAfterSpare 
+                rollIndex <- rollIndex + 2
+            | roll1, roll2, _ -> 
+                score <- score + roll1 + roll2
+                rollIndex <- rollIndex + 2
         )
         score
 
