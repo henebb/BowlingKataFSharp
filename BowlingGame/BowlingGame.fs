f8d6c2aa3e026c8e5e4e6368a2986e1c5522634b
Henrik Ebbeskog <henrik.ebbeskog@activesolution.se>
2020-12-30 09:21:05 +0100

    Use array comprehension and sum array (to avoid mutable score)

--- BowlingGame/BowlingGame.fs
+++ BowlingGame/BowlingGame.fs
@@ -12,25 +12,28 @@ type Game() =
     
 
     member self.Score() =
-        let mutable score = 0
         let mutable rollIndex = 0
-        // Ten frames in a bowling game
-        [|1..10|]
-        |> Array.iter (fun _ -> 
-            match rolls.[rollIndex], rolls.[rollIndex + 1], rolls.[rollIndex + 2] with
-            // Strike?
-            | 10, firstRollAfterStrike, secondRollAfterStrike -> 
-                score <- score + 10 + firstRollAfterStrike + secondRollAfterStrike
-                rollIndex <- rollIndex + 1
-            // Spare?
-            | 1, 9, rollAfterSpare | 2, 8, rollAfterSpare | 3, 7, rollAfterSpare | 4, 6, rollAfterSpare 
-            | 5, 5, rollAfterSpare 
-            | 9, 1, rollAfterSpare | 8, 2, rollAfterSpare | 7, 3, rollAfterSpare | 6, 4, rollAfterSpare ->
-                score <- score + 10 + rollAfterSpare 
-                rollIndex <- rollIndex + 2
-            | roll1, roll2, _ -> 
-                score <- score + roll1 + roll2
-                rollIndex <- rollIndex + 2
-        )
+        
+        let score =
+            [|
+                // Ten frames in a bowling game:
+                for _ in 1..10 do
+                    // Pattern match a tuple with roll plus the two next rolls:
+                    match rolls.[rollIndex], rolls.[rollIndex + 1], rolls.[rollIndex + 2] with
+                    // Strike?
+                    | 10, firstRollAfterStrike, secondRollAfterStrike -> 
+                        rollIndex <- rollIndex + 1
+                        10 + firstRollAfterStrike + secondRollAfterStrike
+                    // Spare?
+                    | 1, 9, rollAfterSpare | 2, 8, rollAfterSpare | 3, 7, rollAfterSpare | 4, 6, rollAfterSpare 
+                    | 5, 5, rollAfterSpare 
+                    | 9, 1, rollAfterSpare | 8, 2, rollAfterSpare | 7, 3, rollAfterSpare | 6, 4, rollAfterSpare ->
+                        rollIndex <- rollIndex + 2
+                        10 + rollAfterSpare
+                    | roll1, roll2, _ -> 
+                        if roll1 <= 10 then
+                            rollIndex <- rollIndex + 2
+                            roll1 + roll2
+            |]
+            |> Array.sum
         score
-
